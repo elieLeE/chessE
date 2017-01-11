474c8345057abf987735c6abed18c37dcaba261b
le_e <duleu.elie@gmail.com>
2017-01-12 00:46:18 +0100

    Add NbrePiece et modif PieceValue
    NbrePiece => nbre de piece de chaque type au depart pour chaque joueur
    PieceValue => Total prends le nombre de piece de chaque type...

---
+++ src/datas/src/typeDefine/NbrePiece.h
@@ -0,0 +1,25 @@
+/*
+ * NbrePiece.h
+ *
+ *  Created on: 6 janv. 2017
+ *      Author: le_e
+ */
+
+#ifndef DATAS_SRC_TYPEDEFINE_NBREPIECE_H_
+#define DATAS_SRC_TYPEDEFINE_NBREPIECE_H_
+
+namespace datas{
+
+#define NBRE_PION 10
+
+#define NBRE_TOUR 2
+#define NBRE_CAVALIER 2
+#define NBRE_FOU 2
+
+#define NBRE_DAME 1
+#define NBRE_ROI 1
+}
+
+
+
+#endif /* DATAS_SRC_TYPEDEFINE_NBREPIECE_H_ */
